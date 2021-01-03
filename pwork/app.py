e7409bb48e0520526e5b965d130d8c2807d8955e
liyunpeng <liyunpeng@weidian.com>
2021-01-04 00:14:26 +0800

    add cookiecutter

--- pwork/app.py
+++ pwork/app.py
@@ -1,5 +1,9 @@
 def main():
-    print("hello world!")
+    print("please input such script to start:")
+    print("1. cookiecutter https://github.com/gaianote/python-framework")
+    print("2. pipenv shell")
+    print("3. pipenv install --dev")
+    print('4. pip install -e ".[dev]"')
 
 
 if __name__ == "__main__":
