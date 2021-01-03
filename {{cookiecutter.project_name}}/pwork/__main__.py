519b2f1564f46a6a3b2c043b296d5eb6129cb4c3
liyunpeng <liyunpeng@weidian.com>
2021-01-03 23:53:46 +0800

    add cookiecutter

---
+++ {{cookiecutter.project_name}}/pwork/__main__.py
@@ -0,0 +1,4 @@
+from {{cookiecutter.project_name}}.app import main
+
+if __name__ == "__main__":
+    main()
---
+++ {{cookiecutter.project_name}}/test/test_main.py
@@ -0,0 +1,5 @@
+from {{cookiecutter.project_name}}.app import main
+
+
+def test_app():
+    assert main() is None
